2ba2c732934c92387970159b7b0a4b7555f7e4e2
Nikita "Tehnick <71207932+tehnickge@users.noreply.github.com>
2024-09-24 17:50:10 +0300

    Добавьте файлы проекта.

---
+++ designPatternNET/FactoryMethods.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace designPatternNET.FactoryMethods
+{
+    //абстрактный класс 
+    abstract class IPerson
+    {
+        public IPerson(string name, string age)
+        {
+            Name = name;
+            Age = age;
+        }
+        public abstract string Name { get; set; }
+        public abstract string Age { get; set; }
+    }
+
+
+    //интерфейс содержащий метод (FactoryMethod) возращающий объект нового класса
+    interface IFactoryPerson
+    {
+        public IPerson FactoryMethod(string name, string age); 
+    }
+
+    class Person : IPerson
+    {
+        public Person(string name, string age) : base(name, age) { }
+        public override string Name { get; set; }
+        public override string Age { get; set; }
+
+    }
+
+    class FactoryPerson : IFactoryPerson 
+    {
+        public IPerson FactoryMethod(string name, string age)
+        {
+            return new Person(name, age);
+        }
+    }
+}
---
+++ designPatternNET/Program.cs
@@ -0,0 +1,23 @@
+﻿using designPatternNET.FactoryMethods;
+
+void CheckMsg(string msg)
+{
+    switch(msg)
+    {
+        case "1": // данная ситуация демонстрирует возможность хранения разных классов в одном контейнере и демонстрирует патерн - фабричный метод
+            List<IPerson> persons = new List<IPerson>();
+            FactoryPerson factoryPersons = new FactoryPerson();
+            for(int i = 0; i < 5; i++) { persons.Add(factoryPersons.FactoryMethod("name", "age")); }
+            foreach(var p in persons) { Console.WriteLine(p.Name); }
+            break;
+    }
+}
+
+
+
+string msg = Console.ReadLine();
+while (msg != null)
+{
+    CheckMsg(msg);
+    msg = Console.ReadLine();
+}
